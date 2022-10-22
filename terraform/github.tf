074dcb5ad3153e0c64a0ba83f270b237825ed7d5
Gabriel Aranha <gabriel.aranha.martins@gmail.com>
2022-10-22 10:59:50 -0300

    Fix

--- terraform/github.tf
+++ terraform/github.tf
@@ -1,10 +1,10 @@
 resource "github_repository" "main" {
-  name       = var.repository_name
-  visibility = var.repository_visibility
+  name       = var.github_repository_name
+  visibility = var.github_repository_visibility
   auto_init  = true
 }
 
 resource "github_branch_default" "main" {
   repository = github_repository.main.name
-  branch     = var.branch
+  branch     = var.github_default_branch
 }
