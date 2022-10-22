c597dc6761d14015e6212b47c9f670338819990c
Gabriel Aranha <gabriel.aranha.martins@gmail.com>
2022-10-21 22:54:30 -0300

    Refactor

---
+++ terraform/kubernetes.tf
@@ -0,0 +1,20 @@
+resource "kubernetes_config_map" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapUsers = yamlencode([
+      {
+        userarn  = data.aws_iam_user.read_only_access_user.arn
+        username = data.aws_iam_user.read_only_access_user.user_name
+        groups   = ["system:masters"]
+      }
+    ])
+  }
+
+  depends_on = [
+    aws_eks_cluster.main
+  ]
+}
